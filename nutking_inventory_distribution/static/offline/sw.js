55f00cd1bed2e173952bdb9489dcace1c462456c
sheveioncallender-boop <sheveioncallender@gmail.com>
2026-07-31 13:32:32 -0400

    Delete nutking_inventory_distribution directory

--- nutking_inventory_distribution/static/offline/sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE = 'nutking-offline-shell-v2';
-const ASSETS = ['/nutking/offline', '/nutking_inventory_distribution/static/offline/app.js', '/nutking_inventory_distribution/static/img/nutking_logo.svg', '/nutking/manifest.webmanifest'];
-self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
-self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
-self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(fetch(event.request).then((response) => { const copy = response.clone(); caches.open(CACHE).then((cache) => cache.put(event.request, copy)); return response; }).catch(() => caches.match(event.request).then((cached) => cached || caches.match('/nutking/offline'))));
-});
